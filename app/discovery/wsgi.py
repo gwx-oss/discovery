271eaffa15cd43d602a3c892275da5318b621458
Sandip Nakrnai <sandip.nakrani@karsun-llc.com>
2019-05-01 18:52:40 -0400

    updated wsgi

--- app/discovery/wsgi.py
+++ app/discovery/wsgi.py
@@ -6,8 +6,22 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
+import os
+import time
+import traceback
+import signal
+import sys
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
+try:
+    application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
