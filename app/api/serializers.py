bcce13f0c3d814d10a11d397f8ef5472cd723eaa
Dinesh Somalaraju <dinesh.somalaraju@karsun-llc.com>
2019-11-15 14:20:01 -0500

    Build failure updated

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -414,17 +414,17 @@ class BasePoolMembershipSerializer(ModelSerializer):
         if vehicle in capabilitiesUrls and duns in capabilitiesUrls[vehicle]:
             return capabilitiesUrls[vehicle][duns]
 
-    # def get_capability_statement(self, item):
-    #     request = self.context.get('request')
-    #     duns = item.vendor.duns
-    #     vehicle = item.pool.vehicle.id
-    #     cs_path = "static/discovery_site/capability_statements/{}/{}.pdf".format(vehicle, duns)
-    #     cs_url = request.build_absolute_uri("/discovery_site/capability_statements/{}/{}.pdf".format(vehicle, duns))
+    def get_capability_statement(self, item):
+        request = self.context.get('request')
+        duns = item.vendor.duns
+        vehicle = item.pool.vehicle.id
+        cs_path = "static/discovery_site/capability_statements/{}/{}.pdf".format(vehicle, duns)
+        cs_url = request.build_absolute_uri("/discovery_site/capability_statements/{}/{}.pdf".format(vehicle, duns))
      
-    #     if vehicle and os.path.isfile(cs_path):
-    #         return cs_url
-    #     else:
-    #         return self.get_capabilities(vehicle, duns)
+        if vehicle and os.path.isfile(cs_path):
+            return cs_url
+        else:
+            return self.get_capabilities(vehicle, duns)
 
     def get_contacts(self, item):
         queryset = vendors.Contact.objects.filter(responsibility=item).order_by('order')
