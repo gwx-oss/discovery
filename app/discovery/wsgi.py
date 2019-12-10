72c43827fab54bba7dbf4de5efd2ea6d12bd0f85
Dinesh Somalaraju <dinesh.somalaraju@karsun-llc.com>
2019-12-10 11:45:46 -0500

    added django version 2.0.11(old)

--- app/discovery/wsgi.py
+++ app/discovery/wsgi.py
@@ -9,10 +9,27 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+# import os
+# import time
+# import traceback
+# import signal
+# import sys
+# #from django.core.wsgi import get_wsgi_application
 
-#application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '': 
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
+# try:
+#     application = get_wsgi_application()
+#     print ('****************WSGI without exception*****************')
+# except Exception:
+#     print ('################handling WSGI exception#############')
+#     # Error loading applications
+#     if 'mod_wsgi' in sys.modules:
+#         traceback.print_exc()
+#         os.kill(os.getpid(), signal.SIGINT)
+#         time.sleep(2.5)
+
+application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
+# # def application(environ, start_response):
+# #     if environ['mod_wsgi.process_group'] != '': 
+# #         import signal
+# #         os.kill(os.getpid(), signal.SIGINT)
+# #     return ["killed"]
