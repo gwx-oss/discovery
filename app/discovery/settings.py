6f31ec71ae55f123e3f20d42c4e5e6aa27344cac
Teddy Desta <theodros.desta@gsa.gov>
2020-04-20 15:48:06 -0400

    [BRMP-380] Fixing /docs 4

--- app/discovery/settings.py
+++ app/discovery/settings.py
@@ -184,7 +184,7 @@ TEMPLATES = [
 STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "src/docs"),
+    os.path.join(BASE_DIR, "frontend/src/docs"),
 )
 
 #
