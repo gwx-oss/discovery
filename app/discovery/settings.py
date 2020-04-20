6718e91e80ed2efe24b0fb7e94c26b9363194826
Teddy Desta <theodros.desta@gsa.gov>
2020-04-20 11:40:01 -0400

    [BRMP-380] Fixing /docs static url in Django settings2

--- app/discovery/settings.py
+++ app/discovery/settings.py
@@ -184,7 +184,6 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "frontend/src/docs"),
 )
 
