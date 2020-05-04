09cbd5e8bc727ee45d0c840a12f267ff1cb53966
Teddy Desta <theodros.desta@gsa.gov>
2020-05-03 22:06:03 -0400

    [BRMP-670] /docs work, but now fixing / pII

--- app/discovery/wsgi.py
+++ app/discovery/wsgi.py
@@ -6,8 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
+import os
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'), index_file=True)
+application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
+application.add_files(os.path.join(settings.BASE_DIR, 'static/docs'), index_file=True)
