7a4eaa5511b69f8f8dbb51e71f16b48bb6f9cba3
Teddy Desta <theodros.desta@gsa.gov>
2020-05-05 16:29:55 -0400

    [BRMP-670] / homepage fix test

--- app/discovery/wsgi.py
+++ app/discovery/wsgi.py
@@ -13,5 +13,26 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 application = WhiteNoise(get_wsgi_application(), root="{}/{}".format(settings.BASE_DIR, 'static'))
-application.add_files(os.path.join(settings.BASE_DIR, 'static/docs'))
+application.add_files(settings.BASE_DIR, 'static/docs')
 application.index_file = True
+
+# Logging WSGI middleware.
+
+import pprint
+
+class LoggingMiddleware:
+
+    def __init__(self, application):
+        self.__application = application
+
+    def __call__(self, environ, start_response):
+        errors = environ['wsgi.errors']
+        pprint.pprint(('REQUEST', environ), stream=errors)
+
+        def _start_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errors)
+            return start_response(status, headers, *args)
+
+        return self.__application(environ, _start_response)
+
+application = LoggingMiddleware(application)
