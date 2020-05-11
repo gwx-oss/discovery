a8badb910b1298bcc8cb9c86701649e63e6fd3ad
chinchalinchin <chinchalinchin@gmail.com>
2020-05-11 19:29:46 -0400

    [BRMP-1699] added echo output to track process during initialization

--- scripts/init-docs.sh
+++ scripts/init-docs.sh
@@ -3,16 +3,23 @@
 
 set -e
 
+#-------------------------------------------------------------------------------
 GH_PAGES_BRANCH="gh-pages"
 GH_PAGES_REMOTE="git@github.com:PSHCDevOps/discovery.git"
-
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+#-------------------------------------------------------------------------------
+
+echo "navigating to $SCRIPT_DIR/../app/static"
 cd "$SCRIPT_DIR/../app/static"
 
+echo "creating /docs/ directory"
 mkdir "docs"
 
+echo "cloning generated documentation"
 git clone -b $GH_PAGES_BRANCH --single-branch GH_PAGES_REMOTE
 
+echo "moving generated documentation into $SCRIPT_DIR/../app/static/docs"
 mv "discovery/docs/html"/* "docs"
 
+echo "cleaning up cloned repo"
 rm -Rf "discovery"
