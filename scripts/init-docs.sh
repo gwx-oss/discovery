7161b477d253db565aad0a281b99af6a69641277
chinchalinchin <chinchalinchin@gmail.com>
2020-05-11 19:43:53 -0400

    [BRMP-1699] cleaning up /docs/ directory before cloning repo

--- scripts/init-docs.sh
+++ scripts/init-docs.sh
@@ -6,17 +6,26 @@ set -e
 #-------------------------------------------------------------------------------
 GH_PAGES_BRANCH="gh-pages"
 GH_PAGES_REMOTE="git@github.com:PSHCDevOps/discovery.git"
+#Testing locally directory
+#SCRIPT_DIR=./
+#Cloud directory
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 #-------------------------------------------------------------------------------
 
 echo "navigating to $SCRIPT_DIR/../app/static"
 cd "$SCRIPT_DIR/../app/static"
 
-echo "creating /docs/ directory"
-mkdir "docs"
+if [ -d "docs/" ] 
+then
+    echo "cleaning /docs/ subdirectory"
+    rm -r "docs"/*
+else
+    echo "creating /docs/ subdirectory"
+    mkdir "docs"
+fi
 
 echo "cloning generated documentation"
-git clone -b $GH_PAGES_BRANCH --single-branch GH_PAGES_REMOTE
+git clone -b $GH_PAGES_BRANCH --single-branch $GH_PAGES_REMOTE
 
 echo "moving generated documentation into $SCRIPT_DIR/../app/static/docs"
 mv "discovery/docs/html"/* "docs"
