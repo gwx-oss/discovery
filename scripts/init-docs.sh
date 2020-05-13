7c690d4baac1bdcc2e5c8127a87952efa5bd8edd
chinchalinchin <chinchalinchin@gmail.com>
2020-05-13 12:39:06 -0400

    testing init-doc.sh

--- scripts/init-docs.sh
+++ scripts/init-docs.sh
@@ -15,6 +15,12 @@ SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || 
 echo "navigating to $SCRIPT_DIR/../app/static"
 cd "$SCRIPT_DIR/../app/static"
 
+LOG_FILE="${1:-$SCRIPT_DIR/../logs/discovery-init.log}"
+if [ "$LOG_FILE" != "/dev/stdout" -a "$LOG_FILE" != "/dev/stderr" ]
+then
+  rm -f "$LOG_FILE"
+fi
+
 # Testing with /docs2/
 if [ -d "docs2/" ] 
 then
