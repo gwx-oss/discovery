eb88a86b96738050b28e9156fa5a82db6ff9ee63
chinchalinchin <chinchalinchin@gmail.com>
2020-06-10 11:32:24 -0400

    configure elasticsearch url

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM debian
+LABEL maintainer.name="Grant Moore" \
+maintainer.email="chinchalinchin@gmail.com" \
+description="A kibana instance configured for CloudFoundry deployment process"
 
 RUN apt update -y && apt upgrade -y && \
 apt install -y wget jq
@@ -8,11 +11,9 @@ WORKDIR /home/
 RUN wget https://artifacts.elastic.co/downloads/kibana/kibana-5.6.0-amd64.deb
 RUN dpkg -i kibana-5.6.0-amd64.deb
 
-USER kibana 
-
 EXPOSE 5601
 
 COPY init-kibana.sh /home/init-kibana.sh
 COPY kibana.yml /etc/kibana/kibana.yml
 
-CMD [ "./init-kibana.sh" ]
+CMD [ "bash", "./init-kibana.sh" ]
--- init-kibana.sh
+++ init-kibana.sh
@@ -1,8 +1,24 @@
-echo "> ElasticSearch URI: ${ELASTICSEARCH_URI}"
+es_uri="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.uri)"
+es_url="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.hostname)"
+es_username="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.username)"
+es_password="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.password)"
+es_port="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.port)"
+
+echo "> ElasticSearch URI: \"${es_uri}\""
+echo "> ElasticSearch URL: \"${es_url}\""
+echo "> ElasticSearch Username: \"${es_username}\""
+echo "> ElasticSearch Password: \"${es_password}\""
+echo "> ElasticSearch Port:\"${es_port}\""
+
+#uri = http://username:password@url:port
 echo "> Configuring kibana.yml with uri..."
 
-sed "s/elasticsearch\.url:/elasticsearch.url:${ELASTICSEARCH_URI}/" /etc/kibana/kibana.yml
+sed -i "s/elasticsearch.url:/elasticsearch.url: \"http:\/\/${es_username}:${es_password}@${es_url}:${es_port}\"/" /etc/kibana/kibana.yml
 
-echo "Kibana.yml configured. Starting Kibana..."
+cat /etc/kibana/kibana.yml
+
+echo "> Kibana.yml configured. Switching to kibana user..."
+
+echo "> Starting kibana..."
 
 bash /usr/share/kibana/bin/kibana
