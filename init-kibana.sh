64059b24c773f791f50fef5b889a0829c1c08d45
chinchalinchin <chinchalinchin@gmail.com>
2020-06-10 12:22:42 -0400

    configure elasticsearch credentials

--- init-kibana.sh
+++ init-kibana.sh
@@ -4,16 +4,17 @@ es_username="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.u
 es_password="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.password)"
 es_port="$(echo "${VCAP_SERVICES}" | jq -r .elasticsearch56[0].credentials.port)"
 
-echo "> ElasticSearch URI: \"${es_uri}\""
 echo "> ElasticSearch URL: \"${es_url}\""
 echo "> ElasticSearch Username: \"${es_username}\""
 echo "> ElasticSearch Password: \"${es_password}\""
 echo "> ElasticSearch Port:\"${es_port}\""
 
 #uri = http://username:password@url:port
-echo "> Configuring kibana.yml with uri..."
+echo "> Configuring kibana.yml with ElasticSearch URI..."
 
 sed -i "s/elasticsearch.url:/elasticsearch.url: \"http:\/\/${es_username}:${es_password}@${es_url}:${es_port}\"/" /etc/kibana/kibana.yml
+sed -i "s/elasticsearch.username:/elasticsearch.username: \"${es_username}\"/" /etc/kibana/kibana.yml
+sed -i "s/elasticsearch.password:/elasticsearch.password: \"${es_password}\"/" /etc/kibana/kibana.yml
 
 cat /etc/kibana/kibana.yml
 
