69c109b8633222545d6dcef91e31ccf51415e2fe
chinchalinchin <chinchalinchin@gmail.com>
2020-09-29 14:13:19 -0400

    testing discovery circleci pipeline

--- app/discovery/settings.py
+++ app/discovery/settings.py
@@ -23,6 +23,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJ_DIR = os.path.dirname(BASE_DIR)
 
+print("here is a change")
 #
 # API settings
 #
